516dd27e87558250ff6f60940e5f118d99b88922
IRASUBIZA Elyse <12356789+odin@users.norely.github.com>
2023-07-13 21:00:57 +0200

    solved the first 2 questions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
-};
 
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  let times = 3;
+  let word = 'hey';
+  console.log(repeatString);
 // Do not edit below this line
 module.exports = repeatString;
