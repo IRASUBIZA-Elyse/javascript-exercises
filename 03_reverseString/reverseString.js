c5d0ebd6755d68d777f55ca44ee432e27e86d83b
IRASUBIZA Elyse <12356789+odin@users.norely.github.com>
2023-07-17 19:46:08 +0200

    answered qn 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function(string1) {
-  return string1.reverse()
-};
-let string1 = "hello"
+function reverseString(string1) {
+  return string1.split("").reverse().join("");
+}
+let string1 = ""
 // Do not edit below this line
 module.exports = reverseString;
